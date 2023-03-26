7a4e6f6ec14866810d8f049559617306ebfab310
VuslatYolcu <ylcvalt@gmail.com>
2023-03-26 19:21:42 +0300

    Add question model

---
+++ MVC-Exam-Questions/Models/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  MVC-Exam-Questions
+//
+//  Created by Vuslat Yolcu on 26.03.2023.
+//
+
+import Foundation
+
+struct Question {
+    
+    var text: String
+    var point: Double
+    var isCorrect: Bool
+}
