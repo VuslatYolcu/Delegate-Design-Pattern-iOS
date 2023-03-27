7e91016f7ad43355ab21bab758e7d8922b06ae3f
VuslatYolcu <ylcvalt@gmail.com>
2023-03-27 14:44:39 +0300

    Add new question view controller

---
+++ MVC-Exam-Questions/Controllers/AddQuestionTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddQuestionTableViewController.swift
+//  MVC-Exam-Questions
+//
+//  Created by Vuslat Yolcu on 27.03.2023.
+//
+
+import Foundation
+import UIKit
+
+class AddQuestionTableViewController: UITableViewController {
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var pointTextField: UITextField!
+    @IBOutlet weak var isCorrectSegmentedControl: UISegmentedControl!
+    
+    @IBAction func close() {
+        
+    }
+    
+    @IBAction func save() {
+        
+    }
+    
+}
