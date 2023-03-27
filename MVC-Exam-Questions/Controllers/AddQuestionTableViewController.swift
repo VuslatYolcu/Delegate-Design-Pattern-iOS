454bf99d3020647c2b9b3575601317e4a8b15e53
VuslatYolcu <ylcvalt@gmail.com>
2023-03-27 16:16:37 +0300

    Add custom delegate

--- MVC-Exam-Questions/Controllers/AddQuestionTableViewController.swift
+++ MVC-Exam-Questions/Controllers/AddQuestionTableViewController.swift
@@ -8,18 +8,41 @@
 import Foundation
 import UIKit
 
+protocol AddQuestionDelegate {
+    func addQuestionDidSaveQuestion(question: Question, controller: UIViewController)
+}
+
 class AddQuestionTableViewController: UITableViewController {
     
     @IBOutlet weak var questionTextField: UITextField!
     @IBOutlet weak var pointTextField: UITextField!
     @IBOutlet weak var isCorrectSegmentedControl: UISegmentedControl!
     
+    var delegate: AddQuestionDelegate!
+    
+    private var isCorrect: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.isCorrectSegmentedControl.addTarget(self, action: #selector(isCorrectSegmentedControlSelected), for: .valueChanged)
+    }
+    
+    @objc func isCorrectSegmentedControlSelected(segmentedControl: UISegmentedControl) {
+        isCorrect = segmentedControl.selectedSegmentIndex == 0 ? true : false
+    }
+    
     @IBAction func close() {
         
     }
     
     @IBAction func save() {
+        let text = questionTextField.text!
+        let point = Double(pointTextField.text!)!
         
+        let question = Question(text: text, point: point, isCorrect: isCorrect)
+        
+        delegate.addQuestionDidSaveQuestion(question: question, controller: self)
     }
     
 }
